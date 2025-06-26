185c003741a38f888ddb9c20062b29a7161de64d
Tethys Neuroplexus <isneuroplexus@duck.com>
2025-06-26 22:06:51 +1000

    style(middleware): remove unused timeFormat constant in token-rate-limit.go

--- middleware/token-rate-limit.go
+++ middleware/token-rate-limit.go
@@ -15,7 +15,6 @@ import (
 const (
 	TokenRateLimitCountMark        = "TRRL"
 	TokenRateLimitSuccessCountMark = "TRRLS"
-	timeFormat                     = "2006-01-02 15:04:05"
 )
 
 func tokenCheckRedisRateLimit(ctx context.Context, rdb *redis.Client, key string, maxCount int, duration int64) (bool, error) {
